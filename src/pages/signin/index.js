bfbc1801e8591cfdb3f5ddeec47970dd85cf1346
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-01-31 22:45:14 +0100

    Add signin form

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 import signinImg from './images/signin.png';
-import { Link } from 'react-router-dom';
 // UI components
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
+import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
 import { ThemeProvider } from '@mui/material/styles';
 // Components
@@ -12,56 +15,92 @@ import PanelWithImage from '../../components/panel-with-image';
 import enLocale from './locales/en.js';
 import Main from '../../themes/main';
 import routes from '../../routes';
+import { fieldElementProps } from '../../utils';
 
-const signInForm = (
-  <Grid container direction="row" justifyContent="center" alignItems="stretch" pt="40px">
-    <Grid item direction="column" justifyContent="center" xs={12} md={10}>
-      <Box
-        component="form"
-        pb="60px"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '100%' }
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField required id="email" label={`${enLocale.form.email}`} color="secondary" focused />
-        <TextField
-          required
-          id="password"
-          label={`${enLocale.form.password}`}
-          color="secondary"
-          focused
-        />
+import { FormContainer, TextFieldElement, PasswordElement } from 'react-hook-form-mui';
 
-        <Button
-          href="#"
-          key="signin-form-submit"
-          variant="contained"
-          color="primary"
-          size="large"
-          sx={{ fontWeight: 'medium' }}
-        >
-          {enLocale.form.submit}
-        </Button>
-      </Box>
-      <Box pb="60px">
-        <Link
-          style={{
-            color: `${Main.palette.primary.main}`,
-            textDecoration: 'none',
-            fontWeight: 'bold'
-          }}
-          to={routes.resetPassword}
-        >
-          {enLocale.forgotPassword}
-        </Link>
-      </Box>
-    </Grid>
-  </Grid>
-);
+const httClient = axios.create({
+  baseURL: 'http://dev.api.yourplantcare.com/v1',
+  timeout: 1000
+});
 
 export const Signin = ({}) => {
+  const history = useNavigate();
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [errors, setErrors] = useState({});
+  const formLocales = enLocale.form;
+  const submit = (data) => {
+    const { email, password } = data;
+    const params = { email, password, locale: 'en' };
+
+    httClient
+      .get('users/signin', { params })
+      .then(function () {
+        history(routes.signupConfirmation);
+      })
+      .catch(function (error) {
+        if (error.response.status === 422) {
+          const { message: responseMessage, errors: responseErrors } = error.response.data;
+          setErrorMessage(responseMessage);
+          setErrors(responseErrors);
+        } else {
+          const responseMessage = error.response.statusText;
+          setErrorMessage(`HTTP error: ${responseMessage}`);
+        }
+      });
+  };
+
+  const form = (
+    <Grid container direction="row" justifyContent="center" alignItems="stretch" pt="40px">
+      <Grid item direction="column" justifyContent="center" xs={12} md={10}>
+        <Box
+          pb="60px"
+          sx={{
+            '& .MuiTextField-root': { mt: 1, mb: 1, width: '100%' }
+          }}
+        >
+          <FormContainer
+            onSuccess={(data) => submit(data)}
+            sx={{ display: 'flex', flexDirection: 'column' }}
+          >
+            {errorMessage && (
+              <Alert severity="error" sx={{ mt: 1, mb: 1 }}>
+                {errorMessage}
+              </Alert>
+            )}
+
+            <TextFieldElement {...fieldElementProps('email', errors, formLocales)} required />
+            <PasswordElement {...fieldElementProps('password', errors, formLocales)} required />
+
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              size="large"
+              sx={{ fontWeight: 'medium' }}
+            >
+              {enLocale.form.submit}
+            </Button>
+          </FormContainer>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+  const forgotPassword = (
+    <Box pb="60px">
+      <Link
+        style={{
+          color: `${Main.palette.primary.main}`,
+          textDecoration: 'none',
+          fontWeight: 'bold'
+        }}
+        to={routes.resetPassword}
+      >
+        {enLocale.forgotPassword}
+      </Link>
+    </Box>
+  );
+
   return (
     <ThemeProvider theme={Main}>
       <Box sx={{ mt: { xs: 5, sm: 8 } }}>
@@ -69,9 +108,11 @@ export const Signin = ({}) => {
           image={signinImg}
           title={enLocale.title}
           subtitle={enLocale.subtitle}
-          children={signInForm}
           styles={{ textAlign: 'center' }}
-        />
+        >
+          {form}
+          {forgotPassword}
+        </PanelWithImage>
       </Box>
     </ThemeProvider>
   );
--- src/pages/signin/index.test.js
+++ src/pages/signin/index.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Route, Router, Routes } from 'react-router-dom';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import axios from 'axios';
 
 import Signin from './index.js';
 
@@ -29,4 +30,88 @@ test('load and render component', () => {
 
   const forgotPasswordLink = screen.getAllByText(/Forgot password\?/i)[0];
   expect(forgotPasswordLink).toBeInTheDocument();
+});
+
+test('render and submit form', async () => {
+  const formParams = {
+    locale: 'en',
+    email: 'luihbautista@gmail.com',
+    password: '12345'
+  };
+  const errorResponse = {
+    response: {
+      status: 404,
+      data: {}
+    }
+  };
+
+  const history = createMemoryHistory();
+  history.push('/signin');
+
+  render(
+    <Router location={history.location} navigator={history}>
+      <App />
+    </Router>
+  );
+
+  axios.get.mockResolvedValueOnce(() => Promise.resolve({ data: {} }));
+
+  const emailInput = screen.getAllByTestId('email-input')[0];
+  const passwordInput = screen.getAllByTestId('password-input')[0];
+
+  expect(emailInput).not.toBeNull();
+  expect(passwordInput).not.toBeNull();
+
+  fireEvent.change(emailInput, { target: { value: formParams.email } });
+  fireEvent.change(passwordInput, { target: { value: formParams.password } });
+
+  const submitButton = screen.getAllByText('Submit')[0];
+  await waitFor(() => {
+    submitButton.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+  });
+
+  expect(axios.get).toHaveBeenCalledWith('users/signin', { params: formParams });
+});
+
+test('render and submit form fails', async () => {
+  const formParams = {
+    locale: 'en',
+    email: 'luihbautista@gmail.com',
+    password: '12345'
+  };
+  const errorResponse = {
+    response: {
+      status: 404,
+      statusText: 'Not found',
+      data: {}
+    }
+  };
+
+  const history = createMemoryHistory();
+  history.push('/signin');
+
+  render(
+    <Router location={history.location} navigator={history}>
+      <App />
+    </Router>
+  );
+
+  axios.get.mockRejectedValueOnce(errorResponse);
+
+  const emailInput = screen.getAllByTestId('email-input')[0];
+  const passwordInput = screen.getAllByTestId('password-input')[0];
+
+  expect(emailInput).not.toBeNull();
+  expect(passwordInput).not.toBeNull();
+
+  fireEvent.change(emailInput, { target: { value: formParams.email } });
+  fireEvent.change(passwordInput, { target: { value: formParams.password } });
+
+  const submitButton = screen.getAllByText('Submit')[0];
+  await waitFor(() => {
+    submitButton.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+  });
+
+  expect(axios.get).toHaveBeenCalledWith('users/signin', { params: formParams });
+  expect(screen.getAllByText('HTTP error: Not found')).toBeDefined();
 });
