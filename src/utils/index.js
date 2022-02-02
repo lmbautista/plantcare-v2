2b47522e5f6f43329cee1e1df51b4804a6b1b06f
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-02-02 16:23:12 +0100

    Add utils to handle encrypted cookies

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,4 +18,39 @@ export const fieldElementProps = (fieldName, errors, locales) => {
   };
 };
 
-export default fieldElementProps;
+import Cookies from 'js-cookie';
+import CryptoJS from 'crypto-js';
+import routes from '../routes';
+
+const pass = 'my-secret-pass'; // TODO: move to env var
+const sessionExpiredIn30minutes = 1 / 48;
+
+const encrypt = (value) => (value === undefined ? undefined : CryptoJS.AES.encrypt(value, pass));
+const decrypt = (value) =>
+  value === undefined ? undefined : CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt(value, pass));
+
+export const setSessionCookies = (session, history) => {
+  const { email, api_token: apiToken } = session;
+
+  Cookies.set('user', encrypt(email), { expires: sessionExpiredIn30minutes });
+  Cookies.set('token', encrypt(apiToken), { expires: sessionExpiredIn30minutes });
+
+  history(routes.signupConfirmation);
+};
+
+export const getSessionCookies = () => {
+  const sessionCookie = {
+    email: decrypt(Cookies.get('user')),
+    token: decrypt(Cookies.get('token'))
+  };
+
+  return JSON.stringify(sessionCookie) === JSON.stringify({}) ? {} : sessionCookie;
+};
+
+export const removeSessionCookies = (history) => {
+  Cookies.remove('user');
+  Cookies.remove('token');
+  history(routes.home);
+};
+
+export const hasSession = () => JSON.stringify(getSessionCookies()) !== JSON.stringify({});
--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,4 +1,4 @@
-import { fieldElementProps } from './index';
+import * as Utils from './index';
 
 test('fieldElementProps with errors', () => {
   const expectedProps = {
@@ -11,7 +11,65 @@ test('fieldElementProps with errors', () => {
     focused: true
   };
 
-  const props = fieldElementProps('fieldName', { fieldName: 'error' }, { fieldName: 'Fieldname' });
+  const props = Utils.fieldElementProps(
+    'fieldName',
+    { fieldName: 'error' },
+    { fieldName: 'Fieldname' }
+  );
 
   expect(JSON.stringify(props)).toEqual(JSON.stringify(expectedProps));
+});
+
+const history = jest.fn();
+const responseHTTP = { email: 'lmiguelbautista@gmail.com', api_token: 'abc123' };
+const expectedSession = { email: responseHTTP.email, token: responseHTTP.api_token };
+
+import routes from '../routes';
+import Cookies from 'js-cookie';
+import CryptoJS from 'crypto-js';
+
+beforeEach(() => jest.clearAllMocks());
+
+test('getSessionCookies', () => {
+  Utils.setSessionCookies(responseHTTP, history);
+  const session = Utils.getSessionCookies();
+
+  expect(JSON.stringify(session)).toEqual(JSON.stringify(expectedSession));
+});
+
+test('removeSessionCookies', () => {
+  Utils.setSessionCookies(responseHTTP, history);
+  let session = Utils.getSessionCookies();
+
+  expect(JSON.stringify(session)).toEqual(JSON.stringify(expectedSession));
+
+  Utils.removeSessionCookies(history);
+  session = Utils.getSessionCookies();
+
+  expect(history).toHaveBeenCalledWith(routes.home);
+  expect(JSON.stringify(session)).toEqual(JSON.stringify({}));
+});
+
+test('hasSession returns true', () => {
+  Utils.removeSessionCookies(history);
+  Utils.setSessionCookies(responseHTTP, history);
+  expect(Utils.hasSession()).toBeTruthy();
+});
+
+test('hasSession returns false', () => {
+  Utils.removeSessionCookies(history);
+  expect(Utils.hasSession()).toBeFalsy();
+});
+
+test('setSessionCookies', () => {
+  Cookies.set = jest.fn();
+  jest.spyOn(CryptoJS.AES, 'encrypt').mockImplementation(() => {
+    return 'encryptedValue';
+  });
+
+  Utils.setSessionCookies(responseHTTP, history);
+
+  expect(history).toHaveBeenCalledWith(routes.signupConfirmation);
+  expect(Cookies.set).toHaveBeenCalledWith('user', 'encryptedValue', { expires: 1 / 48 });
+  expect(Cookies.set).toHaveBeenCalledWith('token', 'encryptedValue', { expires: 1 / 48 });
 });
