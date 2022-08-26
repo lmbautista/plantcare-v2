c9683efe2fd04c1f6c58a3af15ac8a63a34acdc5
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-08-26 16:12:57 +0200

    Add missing tests to submit new/edit form

--- src/pages/plantcares/form/index.test.js
+++ src/pages/plantcares/form/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import axios from 'axios';
-import { LoggedUserContextProvider } from '../../../test_utils';
+import { LoggedUserContextProvider, mockPlantcare } from '../../../test_utils';
 
 import PlantcareForm from './index.js';
 
@@ -38,22 +38,30 @@ test('load and render edit component', () => {
   expect(screen.getAllByText('Submit')).toBeDefined();
 });
 
-test('render and submit new form', async () => {
+test('render and submit edit form', async () => {
+  const plantcareId = 1;
+  const plantcare = { ...mockPlantcare(), id: plantcareId };
+  const onSubmitHandler = jest.fn();
   const formParams = {
+    id: plantcareId,
     name: 'Ficus',
     wet_sensor_field: 'A0',
     water_pump_field: 'IN1'
   };
+  const requestParams = {
+    data: { ...formParams },
+    headers: { Authorization: 'Token asi0o12309djknsdoi8', locale: 'en' },
+    method: 'PUT',
+    url: `plantcares/${plantcareId}`
+  };
 
-  const { container } = render(
+  render(
     <LoggedUserContextProvider section="/form">
-      <App element={<PlantcareForm />} />
+      <App element={<PlantcareForm plantcare={plantcare} onSubmitHandler={onSubmitHandler} />} />
     </LoggedUserContextProvider>
   );
 
-  console.log(container.innerHTML);
-
-  axios.request.mockResolvedValueOnce(() => Promise.resolve({ response: { data: {} } }));
+  axios.request.mockResolvedValue(() => Promise.resolve({ response: { data: {} } }));
 
   const nameInput = screen.getAllByTestId('name-input')[0];
   const wetSensorFieldInput = screen.getAllByTestId('wet-sensor-field-input')[0];
@@ -67,12 +75,56 @@ test('render and submit new form', async () => {
   fireEvent.change(wetSensorFieldInput, { target: { value: formParams.wet_sensor_field } });
   fireEvent.change(waterPumpFieldInput, { target: { value: formParams.water_pump_field } });
 
-  const submitButton = screen.getAllByText('Submit')[0];
   await waitFor(() => {
+    const submitButton = screen.getAllByText('Submit')[0];
     submitButton.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-  });
 
-  // TODO: Find out how to render properly the select options in a test
-  // Otherwise, the form will fail due to a lack of required form values
-  // expect(axios.post).toHaveBeenCalledWith('plantcares', formParams);
+    expect(axios.request).toHaveBeenCalledWith(requestParams);
+    expect(screen.getByText(/Congratulations, everything worked fine!/i)).toBeInTheDocument();
+    expect(onSubmitHandler).toHaveBeenCalledTimes(1);
+  });
+});
+
+test('render and submit new form', async () => {
+  const formParams = {
+    name: 'Ficus',
+    wet_sensor_field: 'A0',
+    water_pump_field: 'IN1'
+  };
+  const requestParams = {
+    data: { id: undefined, ...formParams },
+    headers: { Authorization: 'Token asi0o12309djknsdoi8', locale: 'en' },
+    method: 'POST',
+    url: 'plantcares'
+  };
+  const onSubmitHandler = jest.fn();
+
+  render(
+    <LoggedUserContextProvider section="/form">
+      <App element={<PlantcareForm onSubmitHandler={onSubmitHandler} />} />
+    </LoggedUserContextProvider>
+  );
+
+  axios.request.mockResolvedValue(() => Promise.resolve({ response: { data: {} } }));
+
+  const nameInput = screen.getAllByTestId('name-input')[0];
+  const wetSensorFieldInput = screen.getAllByTestId('wet-sensor-field-input')[0];
+  const waterPumpFieldInput = screen.getAllByTestId('water-pump-field-input')[0];
+
+  expect(nameInput).not.toBeNull();
+  expect(wetSensorFieldInput).not.toBeNull();
+  expect(waterPumpFieldInput).not.toBeNull();
+
+  fireEvent.change(nameInput, { target: { value: formParams.name } });
+  fireEvent.change(wetSensorFieldInput, { target: { value: formParams.wet_sensor_field } });
+  fireEvent.change(waterPumpFieldInput, { target: { value: formParams.water_pump_field } });
+
+  await waitFor(() => {
+    const submitButton = screen.getAllByText('Submit')[0];
+    submitButton.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+
+    expect(axios.request).toHaveBeenCalledWith(requestParams);
+    expect(screen.getByText(/Congratulations, everything worked fine!/i)).toBeInTheDocument();
+    expect(onSubmitHandler).toHaveBeenCalledTimes(1);
+  });
 });
