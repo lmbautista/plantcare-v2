8fabfc27b68dfba25bcc055b4a361b7f5a0e33bb
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-09-05 23:40:53 +0200

    Extract garden stuff in dedicated page component

---
+++ src/pages/plantcares/the-garden.js
@@ -0,0 +1,369 @@
+import { useMemo, useEffect, useState } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+// Images
+import theGardenImg from './images/the-garden.png';
+import plantcaresNotFoundImg from './images/plantcares-not-found.png';
+// UI components
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CloseIcon from '@mui/icons-material/Close';
+import Collapse from '@mui/material/Collapse';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import { ThemeProvider } from '@mui/material/styles';
+// Components
+import Form from './form';
+import PlantcareCard from './card';
+// Others
+import enLocale from './locales/en.js';
+import enFormLocale from './form/locales/en.js';
+import Statics from './statics';
+import { loadingFragment, authHeader } from '../../utils';
+import * as PlantcaresApiClient from '../../api-client/plantcares';
+import { notFoundFragment, sectionHeaderFragment } from '.';
+import Main from '../../themes/main';
+
+const HEADER_HEIGHT = 64;
+
+export const TheGarden = ({}) => {
+  const currentAuthHeader = authHeader();
+  const { styles, props } = useMemo(() => Statics(), []);
+  // HTTP component
+  const [loading, setLoading] = useState(false);
+  const [plantcares, setPlantcares] = useState([]);
+  const [isVisibleErrorMessage, setVisibleErrorMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [errors, setErrors] = useState(undefined);
+  const [isVisibleSuccessMessage, setVisibleSuccessMessage] = useState(false);
+  const [successMessageTitle, setSuccessMessageTitle] = useState(undefined);
+  const [successMessage, setSuccessMessage] = useState(undefined);
+  const [creationVisibility, setCreationVisibility] = useState(false);
+  const [editionVisibility, setEditionVisibility] = useState({});
+
+  const toggleCreation = () => setCreationVisibility((value) => !value);
+
+  const toggleEdition = (plantcareId) => {
+    const visibility =
+      (editionVisibility[plantcareId] !== undefined && !editionVisibility[plantcareId]) ?? false;
+
+    setEditionVisibility((value) => ({ ...value, [plantcareId]: visibility }));
+  };
+
+  const savePlantcares = (plantcares) => {
+    setPlantcares(
+      plantcares.reduce((result, plantcare) => {
+        result[plantcare.id] = plantcare;
+        return result;
+      }, {})
+    );
+  };
+
+  const refreshPlantcare = (plantcare) => {
+    setPlantcares((value) => {
+      value[plantcare.id] = { ...value[plantcare.id], ...plantcare };
+      return value;
+    });
+  };
+
+  const removePlantcare = (plantcare) => {
+    setPlantcares((value) => {
+      delete value[plantcare.id];
+      return value;
+    });
+  };
+
+  const getPlantcares = () => {
+    if (currentAuthHeader === null) {
+      return false;
+    }
+
+    const onSuccessHandler = savePlantcares;
+    const onErrorHandler = ({ responseMessage }) => setErrorMessage(responseMessage);
+    const onFinishHandler = () => setLoading(false);
+    const headers = { ...currentAuthHeader };
+
+    setLoading(true);
+    PlantcaresApiClient.getPlantcares({
+      headers,
+      onSuccessHandler,
+      onErrorHandler,
+      onFinishHandler
+    });
+  };
+
+  const createPlantcare = (data) => {
+    if (currentAuthHeader === null) {
+      return false;
+    }
+
+    const onSuccessHandler = () => {
+      setSuccessMessageTitle(enFormLocale.new.success.title);
+      setSuccessMessage(enFormLocale.new.success.description);
+      getPlantcares();
+      setVisibleSuccessMessage(true);
+    };
+    const onErrorHandler = ({ responseMessage, responseErrors }) => {
+      setErrorMessage(responseMessage);
+      setErrors(responseErrors);
+      setVisibleErrorMessage(true);
+    };
+    const onFinishHandler = () => {
+      toggleCreation();
+      setLoading(false);
+    };
+    const headers = { ...currentAuthHeader };
+
+    setLoading(true);
+    PlantcaresApiClient.createPlantcare({
+      data,
+      headers,
+      onSuccessHandler,
+      onErrorHandler,
+      onFinishHandler
+    });
+  };
+
+  const updatePlantcare = (data) => {
+    if (currentAuthHeader === null) {
+      return false;
+    }
+
+    const onSuccessHandler = (plantcare) => {
+      setSuccessMessageTitle(enFormLocale.edit.success.title);
+      setSuccessMessage(enFormLocale.edit.success.description);
+      refreshPlantcare(plantcare);
+      setVisibleSuccessMessage(true);
+    };
+    const onErrorHandler = ({ responseMessage, responseErrors }) => {
+      setErrorMessage(responseMessage);
+      setErrors(responseErrors);
+      setVisibleErrorMessage(true);
+    };
+    const onFinishHandler = () => {
+      toggleEdition(data.id);
+      setLoading(false);
+    };
+    const headers = { ...currentAuthHeader };
+
+    setLoading(true);
+    PlantcaresApiClient.updatePlantcare({
+      data,
+      headers,
+      onSuccessHandler,
+      onErrorHandler,
+      onFinishHandler
+    });
+  };
+
+  const getPlantcare = (id) => {
+    if (currentAuthHeader === null) {
+      return false;
+    }
+    const onSuccessHandler = refreshPlantcare;
+    const onErrorHandler = ({ responseMessage }) => setErrorMessage(responseMessage);
+    const onFinishHandler = () => {
+      toggleEdition(id);
+      setLoading(false);
+    };
+    const headers = { ...currentAuthHeader };
+
+    setLoading(true);
+    PlantcaresApiClient.getPlantcare({
+      id,
+      headers,
+      onSuccessHandler,
+      onErrorHandler,
+      onFinishHandler
+    });
+  };
+
+  const deletePlantcare = (plantcare) => {
+    if (currentAuthHeader === null) {
+      return false;
+    }
+
+    const onSuccessHandler = () => {
+      setSuccessMessageTitle(enLocale.theGarden.remove.success.title);
+      setSuccessMessage(enLocale.theGarden.remove.success.description);
+      setVisibleSuccessMessage(true);
+      removePlantcare(plantcare);
+    };
+    const onErrorHandler = ({ responseMessage }) => {
+      setErrorMessage(responseMessage);
+      setVisibleErrorMessage(true);
+    };
+    const onFinishHandler = () => {
+      setLoading(false);
+    };
+    const headers = { ...currentAuthHeader };
+
+    setLoading(true);
+    PlantcaresApiClient.deletePlantcare({
+      id: plantcare.id,
+      headers,
+      onSuccessHandler,
+      onErrorHandler,
+      onFinishHandler
+    });
+  };
+
+  useEffect(() => {
+    getPlantcares();
+  }, [JSON.stringify(currentAuthHeader)]);
+
+  const formCardFragment = ({ plantcare, onCloseHandler, onSubmitHandler }) => (
+    <Card sx={styles.card}>
+      <Box data-testid="form" sx={{ position: 'relative' }}>
+        <Box sx={styles.cardForm}>
+          <Button
+            {...props.actionButton}
+            data-testid="close-form"
+            onClick={() => {
+              onCloseHandler();
+            }}
+          >
+            <CloseIcon fontSize="inherit" />
+          </Button>
+        </Box>
+        <Form plantcare={plantcare} onSubmitHandler={onSubmitHandler} errors={errors} />
+      </Box>
+    </Card>
+  );
+
+  const successMessageFragment = (
+    <Collapse in={isVisibleSuccessMessage}>
+      <Alert
+        action={
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={() => {
+              setVisibleSuccessMessage(false);
+            }}
+          >
+            <CloseIcon fontSize="inherit" />
+          </IconButton>
+        }
+        sx={{ mb: 2 }}
+      >
+        <AlertTitle>{successMessageTitle}</AlertTitle>
+        {successMessage}
+      </Alert>
+    </Collapse>
+  );
+
+  const errorMessageFragment = errorMessage && (
+    <Collapse in={isVisibleErrorMessage}>
+      <Alert
+        severity="error"
+        action={
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={() => {
+              setVisibleErrorMessage(false);
+            }}
+          >
+            <CloseIcon fontSize="inherit" />
+          </IconButton>
+        }
+        sx={{ mb: 2 }}
+      >
+        <AlertTitle>{enFormLocale.somethingWentWrong}</AlertTitle>
+        {errorMessage}
+      </Alert>
+    </Collapse>
+  );
+
+  const buttons = (
+    <Stack
+      direction="row"
+      spacing={2}
+      pt={6}
+      sx={{ justifyContent: { xs: 'center', sm: 'flex-start' } }}
+    >
+      <Button
+        onClick={() => toggleCreation()}
+        to="#"
+        variant="outlined"
+        color="primary"
+        size="large"
+        data-testid="new-plantcare-button"
+      >
+        {enLocale.theGarden.new}
+      </Button>
+      <AnchorLink style={{ textDecoration: 'none' }} offset={HEADER_HEIGHT} href="#howto">
+        <Button variant="outlined" color="primary" size="large" underline="none">
+          {enLocale.theGarden.howTo}
+        </Button>
+      </AnchorLink>
+    </Stack>
+  );
+
+  return (
+    <ThemeProvider theme={Main}>
+      <Grid
+        container
+        sx={{ margin: 'auto' }}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        maxWidth="xl"
+      >
+        {sectionHeaderFragment({ section: 'theGarden', image: theGardenImg, buttons })}
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          maxWidth="md"
+          sx={{ margin: { xs: '0 10px', sm: 'auto' } }}
+        >
+          <Grid item xs={12}>
+            {errorMessageFragment}
+            {successMessageFragment}
+          </Grid>
+        </Grid>
+        <Grid container direction="row" justifyContent="space-evenly">
+          {loading && loadingFragment()}
+          {creationVisibility &&
+            formCardFragment({
+              onCloseHandler: toggleCreation,
+              onSubmitHandler: createPlantcare
+            })}
+          {!loading &&
+            Object.values(plantcares).length > 0 &&
+            Object.values(plantcares).map((plantcare) => (
+              <>
+                {!editionVisibility[plantcare.id] && (
+                  <PlantcareCard
+                    key={`card-${plantcare.name}`}
+                    plantcare={plantcare}
+                    onEditHandler={() => toggleEdition(plantcare.id)}
+                    onRemoveHandler={() => deletePlantcare(plantcare)}
+                  />
+                )}
+                {editionVisibility[plantcare.id] &&
+                  formCardFragment({
+                    plantcare,
+                    onCloseHandler: () => toggleEdition(plantcare.id),
+                    onSubmitHandler: updatePlantcare
+                  })}
+              </>
+            ))}
+          {!loading &&
+            Object.entries(plantcares).length === 0 &&
+            notFoundFragment(plantcaresNotFoundImg, enLocale.theGarden.plantcaresNotFound)}
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+};
+
+export default TheGarden;
---
+++ src/pages/plantcares/the-garden.test.js
@@ -0,0 +1,193 @@
+import React from 'react';
+import axios from 'axios';
+import { Router } from 'react-router-dom';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { LoggedUserContextProvider } from '../../test_utils';
+import { UserContextProvider } from '../../UserContext';
+import * as PlantcaresApiClient from '../../api-client/plantcares';
+
+import TheGarden from './the-garden.js';
+
+beforeEach(() => jest.clearAllMocks());
+
+const plantcare = {
+  id: 1,
+  name: 'Ficus retusa',
+  wet: 35.2,
+  planted_at: '03/03/2022',
+  watered_at: '03/03/2022 15:35',
+  waterings: [{ programmed_at: '03/03/2022 21:22' }],
+  wet_synced_at: '03/03/2022 16:45',
+  last_connection_at: '03/03/2022 16:45'
+};
+const plantcares = [plantcare];
+
+test('load plantcares successfully', async () => {
+  const response = { status: 200, data: plantcares };
+  axios.request.mockResolvedValueOnce(response);
+
+  render(
+    <LoggedUserContextProvider section="/plantcares">
+      <TheGarden />
+    </LoggedUserContextProvider>
+  );
+
+  await waitFor(() => {
+    expect(screen.queryAllByText('TheGarden not found')).toEqual([]);
+    expect(screen.queryAllByText(plantcares[0].name)).not.toBeNull();
+  });
+});
+
+test('load plantcares and render edit plantcare component', async () => {
+  const response = { status: 200, data: plantcares };
+  axios.request.mockResolvedValueOnce(response);
+
+  render(
+    <LoggedUserContextProvider section="/plantcares">
+      <TheGarden />
+    </LoggedUserContextProvider>
+  );
+
+  await waitFor(() => {
+    expect(screen.queryAllByTestId('edit-button')).not.toEqual([]);
+
+    const editPlantcareButton = screen.queryAllByTestId('edit-button')[0];
+    fireEvent(editPlantcareButton, new MouseEvent('click', { bubbles: true, cancelable: true }));
+
+    expect(screen.queryAllByTestId('form')[0]).toBeVisible();
+    expect(screen.queryAllByTestId('close-form')[0]).toBeVisible();
+  });
+});
+
+test('load plantcares and render new plantcare component', async () => {
+  const response = { status: 200, data: plantcares };
+  axios.request.mockResolvedValueOnce(response);
+
+  render(
+    <LoggedUserContextProvider section="/plantcares">
+      <TheGarden />
+    </LoggedUserContextProvider>
+  );
+
+  await waitFor(() => {
+    expect(screen.queryAllByTestId('new-plantcare-button')[0]).not.toEqual([]);
+
+    const newPlantcareButton = screen.queryAllByTestId('new-plantcare-button')[0];
+    fireEvent(newPlantcareButton, new MouseEvent('click', { bubbles: true, cancelable: true }));
+
+    expect(screen.queryAllByTestId('form')[0]).toBeVisible();
+    expect(screen.queryAllByTestId('close-form')[0]).toBeVisible();
+  });
+});
+
+test('load plantcares and create plantcare', async () => {
+  axios.request.mockResolvedValueOnce({ status: 200, data: [] }); // GET plantcares
+  axios.request.mockResolvedValueOnce({ status: 201, data: {} }); // POST plantcare
+  axios.request.mockResolvedValueOnce({ status: 200, data: plantcares }); // GET plantcares
+
+  render(
+    <LoggedUserContextProvider section="/plantcares">
+      <TheGarden />
+    </LoggedUserContextProvider>
+  );
+
+  expect(screen.queryAllByTestId('plantcare-card')).toHaveLength(0);
+
+  const newPlantcareButton = screen.queryAllByTestId('new-plantcare-button')[0];
+  fireEvent(newPlantcareButton, new MouseEvent('click', { bubbles: true, cancelable: true }));
+
+  const nameInput = screen.getAllByTestId('name-input')[0];
+  const wetSensorFieldInput = screen.getAllByTestId('wet-sensor-field-input')[0];
+  const waterPumpFieldInput = screen.getAllByTestId('water-pump-field-input')[0];
+
+  fireEvent.change(nameInput, { target: { value: 'Ficus' } });
+  fireEvent.change(wetSensorFieldInput, { target: { value: 'A0' } });
+  fireEvent.change(waterPumpFieldInput, { target: { value: 'IN1' } });
+
+  const submitButton = screen.getAllByText('Submit')[0];
+  submitButton.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+
+  await waitFor(() => {
+    expect(screen.queryAllByTestId('plantcare-card')).toHaveLength(1);
+    expect(screen.queryByText('Congratulations, everything worked fine!')).toBeDefined();
+  });
+});
+
+test('load plantcares and update plantcare', async () => {
+  const newPlantcareName = 'Ficus EDITED';
+  axios.request.mockResolvedValueOnce({ status: 200, data: plantcares }); // GET plantcares
+  axios.request.mockResolvedValueOnce({
+    status: 200,
+    data: { ...plantcare, name: newPlantcareName }
+  }); // PUT plantcare
+
+  render(
+    <LoggedUserContextProvider section="/plantcares">
+      <TheGarden />
+    </LoggedUserContextProvider>
+  );
+
+  await waitFor(() => {
+    expect(screen.queryAllByTestId('plantcare-card')).toHaveLength(1);
+
+    const editButton = screen.queryAllByTestId('edit-button')[0];
+    fireEvent(editButton, new MouseEvent('click', { bubbles: true, cancelable: true }));
+
+    const nameInput = screen.getAllByTestId('name-input')[0];
+    fireEvent.change(nameInput, { target: { value: newPlantcareName } });
+
+    const wetSensorFieldInput = screen.getAllByTestId('wet-sensor-field-input')[0];
+    fireEvent.change(wetSensorFieldInput, { target: { value: 'A0' } });
+
+    const waterPumpFieldInput = screen.getAllByTestId('water-pump-field-input')[0];
+    fireEvent.change(waterPumpFieldInput, { target: { value: 'IN1' } });
+
+    const submitButton = screen.getAllByText('Submit')[0];
+    submitButton.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true }));
+  });
+
+  expect(screen.getByText(newPlantcareName)).toBeDefined();
+  expect(screen.queryByText('Congratulations, everything worked fine!')).toBeDefined();
+});
+
+test('delete plantcares successfully', async () => {
+  axios.request.mockResolvedValueOnce({ status: 200, data: plantcares }); // GET plantcares
+  axios.request.mockResolvedValueOnce({ status: 200, data: plantcares }); // DELETE plantcare
+  axios.request.mockResolvedValueOnce({ status: 200, data: [] }); // GET plantcares
+
+  render(
+    <LoggedUserContextProvider section="/plantcares">
+      <TheGarden />
+    </LoggedUserContextProvider>
+  );
+
+  await waitFor(() => {
+    expect(screen.queryAllByTestId('plantcare-card')).toHaveLength(1);
+    expect(screen.queryAllByTestId('remove-button')).not.toEqual([]);
+
+    const deletePlantcareButton = screen.queryAllByTestId('remove-button')[0];
+    fireEvent(deletePlantcareButton, new MouseEvent('click', { bubbles: true, cancelable: true }));
+
+    expect(screen.queryAllByTestId('plantcare-card')).toHaveLength(0);
+    expect(screen.queryByText('Congratulations, everything worked fine!')).toBeDefined();
+  });
+});
+
+test('initial render prevent loading plantcares', () => {
+  const history = createMemoryHistory();
+  history.push('/plantcares');
+
+  const mockPlantcaresApiClient = jest.fn();
+  PlantcaresApiClient.getPlantcares = mockPlantcaresApiClient;
+
+  render(
+    <UserContextProvider>
+      <Router location={history.location} navigator={history}>
+        <TheGarden />
+      </Router>
+    </UserContextProvider>
+  );
+
+  expect(mockPlantcaresApiClient).not.toHaveBeenCalled();
+});
