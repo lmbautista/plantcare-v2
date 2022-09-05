facd066ce018d6ab5d2fbc2b582923035fce16eb
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-09-05 13:01:27 +0200

    Refactor to use API client and simplify form
    We remove two responsibilities:
    
    - Message management after submit
    - API client requests logic

--- src/pages/plantcares/form/index.js
+++ src/pages/plantcares/form/index.js
@@ -1,30 +1,17 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 // UI components
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import CloseIcon from '@mui/icons-material/Close';
-import Collapse from '@mui/material/Collapse';
 import Grid from '@mui/material/Grid';
-import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { ThemeProvider } from '@mui/material/styles';
-
 // Others
 import enLocale from './locales/en.js';
 import Main from '../../../themes/main';
-import { fieldElementProps, authHeader } from '../../../utils';
+import { fieldElementProps } from '../../../utils';
 
 import { FormContainer, TextFieldElement, SelectElement } from 'react-hook-form-mui';
-
-const httClient = axios.create({
-  baseURL: 'http://dev.api.yourplantcare.com/v1',
-  timeout: 5000
-});
 
 const wetSensorOptions = [
   { title: `${enLocale.wetSensor} 1`, id: 'A0' },
@@ -42,104 +29,14 @@ const waterPumpOptions = [
   { title: `${enLocale.waterPump} 4`, id: 'IN4' }
 ];
 
-export const PlantcareForm = ({ plantcare, onSubmitHandler }) => {
-  const currentAuthHeader = authHeader();
-  const [isVisibleSuccessMessage, setVisibleSuccessMessage] = useState(false);
-  const [isVisibleErrorMessage, setVisibleErrorMessage] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(undefined);
-  const [errors, setErrors] = useState({});
+export const PlantcareForm = ({ plantcare, errors, onSubmitHandler }) => {
   const formLocales = enLocale.form;
   const action = Object.keys(plantcare).length > 0 ? 'edit' : 'new';
   const title = enLocale[action].title;
   const subtitle = enLocale[action].subtitle;
 
-  const submitHandler = (formData) => {
-    if (currentAuthHeader === null) {
-      return false;
-    }
-
-    const data = {
-      id: plantcare.id,
-      name: formData.name,
-      wet_sensor_field: formData.wetSensorField,
-      water_pump_field: formData.waterPumpField
-    };
-    const method = plantcare.id ? 'PUT' : 'POST';
-    const url = plantcare.id ? `plantcares/${plantcare.id}` : 'plantcares';
-    const headers = { locale: 'en', ...currentAuthHeader };
-
-    httClient
-      .request({ method, url, data, headers })
-      .then(function () {
-        setVisibleSuccessMessage(true);
-        onSubmitHandler();
-      })
-      .catch(function (error) {
-        if (error.response && error.response.status === 422) {
-          const { message: responseMessage, errors: responseErrors } = error.response.data;
-          setErrorMessage(responseMessage);
-          setErrors(responseErrors);
-        } else {
-          const responseMessage = (error.response && error.response.statusText) || error.message;
-          setErrorMessage(`HTTP error: ${responseMessage}`);
-        }
-        setVisibleErrorMessage(true);
-      });
-  };
-
-  const successMessageFragment = (
-    <Collapse in={isVisibleSuccessMessage}>
-      <Alert
-        action={
-          <IconButton
-            aria-label="close"
-            color="inherit"
-            size="small"
-            onClick={() => {
-              setVisibleSuccessMessage(false);
-            }}
-          >
-            <CloseIcon fontSize="inherit" />
-          </IconButton>
-        }
-        sx={{ mb: 2 }}
-      >
-        <AlertTitle>{enLocale[action].success.title}</AlertTitle>
-        {enLocale[action].success.description}
-      </Alert>
-    </Collapse>
-  );
-
-  const errorMessageFragment = errorMessage && (
-    <Collapse in={isVisibleErrorMessage}>
-      <Alert
-        severity="error"
-        action={
-          <IconButton
-            aria-label="close"
-            color="inherit"
-            size="small"
-            onClick={() => {
-              setVisibleErrorMessage(false);
-            }}
-          >
-            <CloseIcon fontSize="inherit" />
-          </IconButton>
-        }
-        sx={{ mb: 2 }}
-      >
-        <AlertTitle>{enLocale.somethingWentWrong}</AlertTitle>
-        {errorMessage}
-      </Alert>
-    </Collapse>
-  );
-
   const form = (
     <>
-      <Grid item direction="column" justifyContent="center" xs={10}>
-        {successMessageFragment}
-        {errorMessageFragment}
-      </Grid>
       <Grid item direction="column" justifyContent="center" xs={10}>
         <Box
           pb="60px"
@@ -153,7 +50,7 @@ export const PlantcareForm = ({ plantcare, onSubmitHandler }) => {
               wetSensorField: plantcare.wet_sensor_field,
               waterPumpField: plantcare.water_pump_field
             }}
-            onSuccess={(data) => submitHandler(data)}
+            onSuccess={(data) => onSubmitHandler({ ...data, id: plantcare.id })}
             sx={{ display: 'flex', flexDirection: 'column' }}
           >
             <TextFieldElement
@@ -218,10 +115,11 @@ export default PlantcareForm;
 
 PlantcareForm.propTypes = {
   plantcare: PropTypes.object,
+  errors: PropTypes.object,
   onSubmitHandler: PropTypes.func
 };
 
 PlantcareForm.defaultProps = {
   plantcare: {},
-  onSubmitHandler: () => {}
+  errors: {}
 };
--- src/pages/plantcares/form/index.test.js
+++ src/pages/plantcares/form/index.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
-import axios from 'axios';
 import { LoggedUserContextProvider, mockPlantcare } from '../../../test_utils';
 
 import PlantcareForm from './index.js';
 
+const onSubmitHandler = jest.fn();
 const App = ({ element }) => (
   <>
     <Routes>
@@ -14,10 +14,12 @@ const App = ({ element }) => (
   </>
 );
 
+beforeEach(() => jest.clearAllMocks());
+
 test('load and render new component', () => {
   render(
     <LoggedUserContextProvider section="/form">
-      <App element={<PlantcareForm />} />
+      <App element={<PlantcareForm onSubmitHandler={onSubmitHandler} />} />
     </LoggedUserContextProvider>
   );
 
@@ -29,7 +31,9 @@ test('load and render new component', () => {
 test('load and render edit component', () => {
   render(
     <LoggedUserContextProvider section="/form">
-      <App element={<PlantcareForm plantcare={{ name: 'Ficus' }} />} />
+      <App
+        element={<PlantcareForm plantcare={{ name: 'Ficus' }} onSubmitHandler={onSubmitHandler} />}
+      />
     </LoggedUserContextProvider>
   );
 
@@ -38,21 +42,48 @@ test('load and render edit component', () => {
   expect(screen.getAllByText('Submit')).toBeDefined();
 });
 
+test('render and submit new form', async () => {
+  const formParams = {
+    id: undefined,
+    name: 'Ficus',
+    wetSensorField: 'A0',
+    waterPumpField: 'IN1'
+  };
+
+  render(
+    <LoggedUserContextProvider section="/form">
+      <App element={<PlantcareForm onSubmitHandler={onSubmitHandler} />} />
+    </LoggedUserContextProvider>
+  );
+
+  const nameInput = screen.getAllByTestId('name-input')[0];
+  const wetSensorFieldInput = screen.getAllByTestId('wet-sensor-field-input')[0];
+  const waterPumpFieldInput = screen.getAllByTestId('water-pump-field-input')[0];
+
+  expect(nameInput).not.toBeNull();
+  expect(wetSensorFieldInput).not.toBeNull();
+  expect(waterPumpFieldInput).not.toBeNull();
+
+  fireEvent.change(nameInput, { target: { value: formParams.name } });
+  fireEvent.change(wetSensorFieldInput, { target: { value: formParams.wetSensorField } });
+  fireEvent.change(waterPumpFieldInput, { target: { value: formParams.waterPumpField } });
+
+  await waitFor(() => {
+    const submitButton = screen.getAllByText('Submit')[0];
+    submitButton.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+
+    expect(onSubmitHandler).toHaveBeenNthCalledWith(1, formParams);
+  });
+});
+
 test('render and submit edit form', async () => {
   const plantcareId = 1;
   const plantcare = { ...mockPlantcare(), id: plantcareId };
-  const onSubmitHandler = jest.fn();
   const formParams = {
     id: plantcareId,
     name: 'Ficus',
-    wet_sensor_field: 'A0',
-    water_pump_field: 'IN1'
-  };
-  const requestParams = {
-    data: { ...formParams },
-    headers: { Authorization: 'Token asi0o12309djknsdoi8', locale: 'en' },
-    method: 'PUT',
-    url: `plantcares/${plantcareId}`
+    wetSensorField: 'A0',
+    waterPumpField: 'IN1'
   };
 
   render(
@@ -61,8 +92,6 @@ test('render and submit edit form', async () => {
     </LoggedUserContextProvider>
   );
 
-  axios.request.mockResolvedValue(() => Promise.resolve({ response: { data: {} } }));
-
   const nameInput = screen.getAllByTestId('name-input')[0];
   const wetSensorFieldInput = screen.getAllByTestId('wet-sensor-field-input')[0];
   const waterPumpFieldInput = screen.getAllByTestId('water-pump-field-input')[0];
@@ -72,59 +101,13 @@ test('render and submit edit form', async () => {
   expect(waterPumpFieldInput).not.toBeNull();
 
   fireEvent.change(nameInput, { target: { value: formParams.name } });
-  fireEvent.change(wetSensorFieldInput, { target: { value: formParams.wet_sensor_field } });
-  fireEvent.change(waterPumpFieldInput, { target: { value: formParams.water_pump_field } });
+  fireEvent.change(wetSensorFieldInput, { target: { value: formParams.wetSensorField } });
+  fireEvent.change(waterPumpFieldInput, { target: { value: formParams.waterPumpField } });
 
   await waitFor(() => {
     const submitButton = screen.getAllByText('Submit')[0];
     submitButton.dispatchEvent(new MouseEvent('click', { bubbles: true }));
 
-    expect(axios.request).toHaveBeenCalledWith(requestParams);
-    expect(screen.getByText(/Congratulations, everything worked fine!/i)).toBeInTheDocument();
-    expect(onSubmitHandler).toHaveBeenCalledTimes(1);
-  });
-});
-
-test('render and submit new form', async () => {
-  const formParams = {
-    name: 'Ficus',
-    wet_sensor_field: 'A0',
-    water_pump_field: 'IN1'
-  };
-  const requestParams = {
-    data: { id: undefined, ...formParams },
-    headers: { Authorization: 'Token asi0o12309djknsdoi8', locale: 'en' },
-    method: 'POST',
-    url: 'plantcares'
-  };
-  const onSubmitHandler = jest.fn();
-
-  render(
-    <LoggedUserContextProvider section="/form">
-      <App element={<PlantcareForm onSubmitHandler={onSubmitHandler} />} />
-    </LoggedUserContextProvider>
-  );
-
-  axios.request.mockResolvedValue(() => Promise.resolve({ response: { data: {} } }));
-
-  const nameInput = screen.getAllByTestId('name-input')[0];
-  const wetSensorFieldInput = screen.getAllByTestId('wet-sensor-field-input')[0];
-  const waterPumpFieldInput = screen.getAllByTestId('water-pump-field-input')[0];
-
-  expect(nameInput).not.toBeNull();
-  expect(wetSensorFieldInput).not.toBeNull();
-  expect(waterPumpFieldInput).not.toBeNull();
-
-  fireEvent.change(nameInput, { target: { value: formParams.name } });
-  fireEvent.change(wetSensorFieldInput, { target: { value: formParams.wet_sensor_field } });
-  fireEvent.change(waterPumpFieldInput, { target: { value: formParams.water_pump_field } });
-
-  await waitFor(() => {
-    const submitButton = screen.getAllByText('Submit')[0];
-    submitButton.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-
-    expect(axios.request).toHaveBeenCalledWith(requestParams);
-    expect(screen.getByText(/Congratulations, everything worked fine!/i)).toBeInTheDocument();
-    expect(onSubmitHandler).toHaveBeenCalledTimes(1);
+    expect(onSubmitHandler).toHaveBeenNthCalledWith(1, formParams);
   });
 });
